8bda6f75e7f2f81b2e7d3bd66c2815b843f2b0f1
JuanCamiloGomezAlvarez <kamygz93@hotmail.com>
2022-08-05 12:31:56 -0500

    created function renderProduct(), allow to create cards products

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+///////////////////////////////////////////////////////////////////////////
 const menuEmail = document.querySelector(".navbar-email")
 const desktopMenu = document.querySelector(".desktop-menu")
 
@@ -37,3 +38,71 @@ function toggleProducDetail(){
     mobileMenu.classList.add("inactive")
 }
 
+/////////////////////////////////////////////////////////////////////////
+//Creando cards de productos 
+
+const productList = []
+
+productList.push({
+    img:"https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260",
+    name: "Bici-negra-clasica",
+    price: 100,
+})
+
+productList.push({
+    img:"https://images.pexels.com/photos/100582/pexels-photo-100582.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260",
+    name: "Bici-negra-deportiva",
+    price: 150,
+})
+
+productList.push({
+    img:"https://images.pexels.com/photos/2130611/pexels-photo-2130611.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260",
+    name: "Bici-azul-clasica",
+    price: 120,
+})
+
+productList.push({
+    img:"https://images.pexels.com/photos/1595483/pexels-photo-1595483.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260",
+    name: "Bici-blanca-clasica",
+    price: 120,
+})
+
+const cardConstainer = document.querySelector(".cards-container")
+
+
+
+function renderProducts(arr){
+    for (product of arr){
+        const productCard = document.createElement("div")
+        productCard.classList.add("product-card")
+    
+        const productImg = document.createElement("img")
+        productImg.classList.add("product-img")
+        productImg.setAttribute("src", product.img)
+    
+        const productInfo = document.createElement("div")
+        productInfo.classList.add("product-info")
+    
+        const productInfoDiv = document.createElement("div")
+        
+        const productPrice = document.createElement("p")
+        productPrice.innerText = "$" + product.price
+        const productName = document.createElement("p")
+        productName.innerText =  product.name
+    
+        const productFigure = document.createElement("figure")
+        const productImgCart = document.createElement("img")
+        productImgCart.setAttribute("src", "./icons/bt_add_to_cart.svg")
+    
+    
+        productFigure.appendChild(productImgCart)
+        productInfo.appendChild(productInfoDiv)
+        productInfo.appendChild(productFigure)
+        productInfoDiv.append(productPrice, productName)
+        productCard.appendChild(productImg)
+        productCard.appendChild(productInfo)
+        cardConstainer.appendChild(productCard)
+    }
+}
+
+renderProducts(productList)
